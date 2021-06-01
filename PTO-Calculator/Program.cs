7db78d4d6b9063d14f679abb4b921209ea39a7b7
Rick Gray <Rick.Gray@INEIGHT.COM>
2021-06-01 16:56:08 -0500

    create events spread across the year

--- PTO-Calculator/Program.cs
+++ PTO-Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Ical.Net;
 using Ical.Net.CalendarComponents;
 using Ical.Net.DataTypes;
@@ -10,12 +11,48 @@ namespace PtoCalculator
     {
         static void Main()
         {
+            var CURRENT_YEAR = 2021;
+            var PTO_DAYS_EARNED_PER_YEAR = 20.0;
+            var PTO_DAYS_NEEDED_FOR_WINTER_HOLIDAYS = 7;
+
+            //var testText = File.ReadAllText(new DirectoryInfo("test.ics").FullName);
+            //var test = Calendar.Load(testText);
+
             var calendar = new Calendar();
-            calendar.Events.Add(new CalendarEvent
+
+            //timezones: https://nodatime.org/TimeZones
+            calendar.AddTimeZone(new VTimeZone("America/Chicago"));
+
+            var pto_savings_percentage = PTO_DAYS_NEEDED_FOR_WINTER_HOLIDAYS / PTO_DAYS_EARNED_PER_YEAR;
+            var days_till_next_full_pto_day_accrual = 365 / PTO_DAYS_EARNED_PER_YEAR;
+
+            var currentDate = new DateTime(CURRENT_YEAR, 1, 1).AddDays(days_till_next_full_pto_day_accrual);
+            var endDate = currentDate.AddYears(1);
+
+            var dayCount = 1;
+
+            while (currentDate < endDate)
             {
-                Start = new CalDateTime(DateTime.Now),
-                End = new CalDateTime(DateTime.Now.AddDays(1))
-            });
+                var newEvent = new CalendarEvent();
+
+                if (currentDate.Hour > 0 || currentDate.Minute > 0)
+                {
+                    //round to next day
+                    newEvent.Start = new CalDateTime(new DateTime(currentDate.Year, currentDate.Month, currentDate.Day).AddDays(1));
+                }
+                else
+                {
+                    newEvent.Start = new CalDateTime(currentDate);
+                }
+
+                newEvent.End = new CalDateTime(currentDate.AddDays(1));
+                newEvent.Summary = $"{dayCount}/5 PTO Days Accrued";
+
+                calendar.Events.Add(newEvent);
+
+                currentDate = currentDate.AddDays(days_till_next_full_pto_day_accrual);
+                dayCount++;
+            }
 
             var serializer = new CalendarSerializer();
             Console.WriteLine(serializer.SerializeToString(calendar));
