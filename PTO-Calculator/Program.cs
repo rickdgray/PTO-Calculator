887ef98c8a9af936f3529c99d8684d6f604d7a48
Rick Gray <Rick.Gray@INEIGHT.COM>
2021-06-01 15:09:40 -0500

    basic event creation

--- PTO-Calculator/Program.cs
+++ PTO-Calculator/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
 
-namespace PTO_Calculator
+namespace PtoCalculator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var calendar = new Calendar();
+            calendar.Events.Add(new CalendarEvent
+            {
+                Start = new CalDateTime(DateTime.Now),
+                End = new CalDateTime(DateTime.Now.AddDays(1))
+            });
+
+            var serializer = new CalendarSerializer();
+            Console.WriteLine(serializer.SerializeToString(calendar));
         }
     }
 }
