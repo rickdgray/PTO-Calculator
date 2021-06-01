6c28c01b987354389ae152c9ebb48dcd61eb62b1
Rick Gray <Rick.Gray@INEIGHT.COM>
2021-06-01 17:17:03 -0500

    account for PTO savings

--- PTO-Calculator/Program.cs
+++ PTO-Calculator/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using Ical.Net;
 using Ical.Net.CalendarComponents;
 using Ical.Net.DataTypes;
@@ -23,8 +22,7 @@ namespace PtoCalculator
             //timezones: https://nodatime.org/TimeZones
             calendar.AddTimeZone(new VTimeZone("America/Chicago"));
 
-            var pto_savings_percentage = PTO_DAYS_NEEDED_FOR_WINTER_HOLIDAYS / PTO_DAYS_EARNED_PER_YEAR;
-            var days_till_next_full_pto_day_accrual = 365 / PTO_DAYS_EARNED_PER_YEAR;
+            var days_till_next_full_pto_day_accrual = 365 / (PTO_DAYS_EARNED_PER_YEAR  - PTO_DAYS_NEEDED_FOR_WINTER_HOLIDAYS);
 
             var currentDate = new DateTime(CURRENT_YEAR, 1, 1).AddDays(days_till_next_full_pto_day_accrual);
             var endDate = currentDate.AddYears(1);
@@ -35,7 +33,7 @@ namespace PtoCalculator
             {
                 var newEvent = new CalendarEvent();
 
-                if (currentDate.Hour > 0 || currentDate.Minute > 0)
+                if (currentDate.Hour > 0 || currentDate.Minute > 0 || currentDate.Second > 0)
                 {
                     //round to next day
                     newEvent.Start = new CalDateTime(new DateTime(currentDate.Year, currentDate.Month, currentDate.Day).AddDays(1));
